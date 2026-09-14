a3ec8f9bdde2c0de2f643bb0ff0fbc0dbda67926
MorganCod3s <mbelomme@gmail.com>
2026-09-14 10:48:12 -0400

    tink!

---
+++ U1L0.java
@@ -0,0 +1,15 @@
+public class U1L0 {
+    public static void main (String[] args) {
+    System.out.println("|-----------------------------------------|");
+    System.out.println("| My name is Morgan Belomme               |");
+    System.out.println("| I love to sing and draw realism!        |");
+    System.out.println("|-----------------------------------------|");
+
+    }
+}
+// hello dis is a smoodie
+   
+
+    // DOUBLES!! XD
+    // System.out.println("Hello world!");
+    // System.out.println("I really like chocolate fudge cake.");
