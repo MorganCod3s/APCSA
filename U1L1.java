91b4b7bf5f955e8d1514ad06f98b654d5fca05a7
MorganCod3s <mbelomme@gmail.com>
2026-09-14 11:04:00 -0400

    another lesson (second day!!)

---
+++ U1L1.java
@@ -0,0 +1,17 @@
+public class U1L1 {
+    public static void main(String[] args) {
+        System.out.println("Look");
+        System.out.println("at me!");
+        System.out.println("Hi!");
+
+        System.out.println();
+        System.out.print("Look");
+        System.out.print("at me!");
+        System.out.print("Hi!");
+
+     System.out.println();
+        System.out.println();
+        System.out.println("Look at me!");
+        System.out.println("Hi!");
+    }
+}
