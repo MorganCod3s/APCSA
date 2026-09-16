52093a5ddbb074fd7ef2841dd3c87448b6088fa0
MorganCod3s <mbelomme@gmail.com>
2026-09-16 10:56:07 -0400

    morgies third day

--- U1L1.java
+++ U1L1.java
@@ -1,17 +1,45 @@
 public class U1L1 {
     public static void main(String[] args) {
-        System.out.println("Look");
-        System.out.println("at me!");
+        System.out.println();
+        System.out.print("Look");
+        System.out.println(" at me!");
         System.out.println("Hi!");
 
         System.out.println();
-        System.out.print("Look");
-        System.out.print("at me!");
-        System.out.print("Hi!");
+        System.out.print("Look at ");
+        System.out.println("me!");
+        System.out.println("Hi!");
 
      System.out.println();
         System.out.println();
         System.out.println("Look at me!");
         System.out.println("Hi!");
+
+
+        System.out.println("AP CSA");                     // Line 1
+     System.out.println("String literal");             // Line 2
+     System.out.println("A4687BC$");                   // Line 3
+     System.out.println("* * BOOM! * *");              // Line 4
+     System.out.println("1 + 2");                      // Line 5
+     System.out.println("System.out.println");         // Line 6
+     System.out.println("baad speling"); 
+
+
+     System.out.println(27/9);
+     System.out.println(25%3);
+     System.out.println(587-241);
+     System.out.println(12+2);
+     System.out.println(8*7);
+     double base = 15.0;
+     double exponent = 2.0;
+     System.out.println(Math.pow(base, exponent));
+
+     // Math.pow only accepts doubles
+
+
+
+
     }
 }
+
+
