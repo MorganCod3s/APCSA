a2868f52093df282fd67f90c8ca921d37ac3394c
MorganCod3s <mbelomme@gmail.com>
2026-09-16 11:03:10 -0400

    ziggy mentioned!!

---
+++ U1L1TurinCat.java
@@ -0,0 +1,9 @@
+public class U1L1TurinCat {
+    public static void main(String[] args) {
+
+        System.out.println("I have a cat.");
+    System.out.println();
+    System.out.println("His name is Ziggy.");
+// My friend's cats name is also ziggy!!!!! :3
+}
+}
