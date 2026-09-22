145e092b21177a17527c8bf6d2399198ac4fefae
MorganCod3s <mbelomme@gmail.com>
2026-09-22 11:05:12 -0400

    so many problems

--- Unit1/U1T4Problem3.java
+++ Unit1/U1T4Problem3.java
@@ -2,7 +2,7 @@ package Unit1;
 import java.util.Scanner;
 public class U1T4Problem3 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in)
+        Scanner scanner = new Scanner(System.in);
          System.out.print("Enter a 3-digit number: ");
         double num1 = scanner.nextDouble(); 
     }
