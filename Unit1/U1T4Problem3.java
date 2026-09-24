380dccf497dec23d9e706736a6c2550634b948c3
MorganCod3s <mbelomme@gmail.com>
2026-09-24 10:10:08 -0400

    so many problems

--- Unit1/U1T4Problem3.java
+++ Unit1/U1T4Problem3.java
@@ -4,7 +4,16 @@ public class U1T4Problem3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
          System.out.print("Enter a 3-digit number: ");
-        double num1 = scanner.nextDouble(); 
+        int num = scanner.nextInt();
+        int digit1 = num/100;
+        int digit2 = (num % 100)/10;
+        int digit3 = (num % 100)%10;
+        if (digit1 == 0) {
+            System.out.println("The reverse of 0" + num + " 7is: " + digit3 + digit2 + digit1);
+        } else {
+        System.out.println("The reverse of " + num + " 82is: " + digit3 + digit2 + digit1);
+        }
+        scanner.close();
     }
     
 }
