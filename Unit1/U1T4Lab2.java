d6f547d8f476ce561090c73526eab0e6eb943802
MorganCod3s <mbelomme@gmail.com>
2026-09-24 11:05:08 -0400

    wowie

---
+++ Unit1/U1T4Lab2.java
@@ -0,0 +1,33 @@
+package Unit1;
+
+public class U1T4Lab2 {
+    public static void main(String[] args) {
+  /*      int x = 6;              // line 1
+    x += 4;              // line 2
+    System.out.println(x);  // line 3
+                            // line 4
+    int y = 10;             // line 5
+    y *= 5;              // line 6
+    System.out.println(y);  // line 7 */
+
+
+    /* int x = 15;             // line 1
+    x += 1;              // line 2
+    System.out.println(x);  // line 3
+                            // line 4
+    int y = 20;             // line 5
+    y -= 1;              // line 6
+    System.out.println(y);  // line 7 */
+
+    int x = 4;
+        x += 7;
+        x -= 4;
+        x++;
+        x *= 11;
+        x /= 9 + 1;  // add 9 + 1 before dividing! 
+        x--;
+        x %= 3;
+        System.out.println("x = " + x);
+
+    }
+}
